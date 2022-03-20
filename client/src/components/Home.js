3a9b1a5d00831a99691075f70782c9b63536a10e
Piyush <77017010+TigerAtGit@users.noreply.github.com>
2022-03-20 18:17:34 +0530

    Initialize Client side

---
+++ client/src/components/Home.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import getWeb3 from "../getWeb3";
+import ElectionContract from "../contracts/ElectionContract.json";
+
+import "../App.css";
+
+class Home extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      ElectionInstance: undefined,
+      web3: null,
+      accounts: null,
+      isOwner: false
+    };
+  }
+
+
+  componentDidMount = async () => {
+
+    if (!window.location.hash) {
+      window.location = window.location + '#loaded';
+      window.location.reload();
+    }
+    try {
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = ElectionContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        ElectionContract.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({ ElectionInstance: instance, web3: web3, account: accounts[0] });
+      console.log(accounts[0]);
+
+      const owner = await this.state.ElectionInstance.methods.getOwner().call();
+      if (this.state.account === owner) {
+        this.setState({ isOwner: true })
+      }
+
+      let start = await this.state.ElectionInstance.methods.getStart().call();
+      let end = await this.state.ElectionInstance.methods.getEnd().call();
+
+      this.setState({ start: start, end: end })
+
+
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+  };
+
+  render() {
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contract...</div>;
+    }
+    return (
+      <div>
+        Hello Piyush!
+        <div>
+          your user address is {this.state.account}
+        </div>
+        {this.state.isOwner ?
+          <div>Yes you are the owner</div> :
+          <div>No you are not the owner</div>
+        }
+
+      </div>
+    );
+  }
+}
+
+export default Home;
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './components/Home';
 
 import { Router, Switch, Route } from 'react-router-dom';
 import history from './history';
@@ -9,12 +9,8 @@ import history from './history';
 ReactDOM.render(
     <Router history={history}>
         <Switch>
-            <Route exact path='/' component={App} />
+            <Route exact path='/' component={Home} />
         </Switch>
-    </Router>
-
+    </Router>,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
