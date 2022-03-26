8d1f1f705349259e0014419140e9e23e8c4947ac
Piyush <77017010+TigerAtGit@users.noreply.github.com>
2022-03-26 19:54:03 +0530

    Owner account fix

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -11,7 +11,7 @@ class Home extends Component {
     this.state = {
       ElectionInstance: undefined,
       web3: null,
-      accounts: null,
+      account: null,
       isOwner: false
     };
   }
